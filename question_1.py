1313546fa7046e167e7f20585f4cd1e681e9f2fb
Davy Jones <kunal.dekhane@gmail.com>
2024-03-29 22:27:06 -0400

    Latest commit - 29-3-2024

--- question_1.py
+++ question_1.py
@@ -1,21 +1,7 @@
+# import numpy as np
 # Write a class definition for an Image.
 
-sample_image = """
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-##     ## 
-"""
-
-
 class PracticeImage:
-
     # Implement as public functions the constructor,
     def __init__(self):
         # An Image object holds pixel values in a 10x10 grid.
@@ -28,15 +14,13 @@ class PracticeImage:
         self._pixel_status = False
         self._pixel_symbol = "#"
         self._output_string = ""
-
-        # The double underscore __ prefixed to a variable makes it private.
-        # It gives a strong suggestion not to touch it from outside the class.
-        # Any attempt to do so will result in an AttributeError
-
+        self._flipped_values = [[] for i in range(self._grid_size)]
+        self._flipped_output_string = ""
+        self._flopped_values = [[] for i in range(self._grid_size)]
+        self._flopped_output_string = ""
 
 
     # getters and setters for the pixels and symbol,
-
     @property
     def pixel_info(self):
         """
@@ -48,10 +32,10 @@ class PracticeImage:
                 Returns:
                     size and symbol for the pixels
         """
-        print("Getting the pixels info")
-        print(
-            f"Pixel values are: {self._pixel_values}, Pixel status is {self._pixel_status} & pixel symbol is {self._pixel_symbol}")
-        print(f"Grid size is {self._grid_size}")
+        #print("Getting the pixels info")
+        # print(
+        #    f"Pixel values are: {self._pixel_values}, Pixel status is {self._pixel_status} & pixel symbol is {self._pixel_symbol}")
+        # print(f"Grid size is {self._grid_size}")
         return self._pixel_values, self._pixel_status, self._pixel_symbol, self._grid_size
 
     @pixel_info.setter
@@ -60,20 +44,26 @@ class PracticeImage:
             Sets the symbol fon the pixels
 
                 Parameters:
-                    value: Tuple
+                    value: Tuple passed from the main funciton are the pixel number and symbol
 
                 Returns:
                     None
         """
-        print("Setting the pixels attributes")
+        #print("Setting the pixels attributes")
         xPos, yPos, symbol = value
         self._pixel_values[xPos][yPos] = symbol
 
-
-
-
     # along with an output function
     def output_image(self):
+        """
+            Converts the pixel values stored in the image object to string and returns the string
+
+                Parameters:
+                    None
+
+                Returns:
+                    Output image in str format readable on the console.
+        """
         for i in range(self._grid_size):
             for j in range (self._grid_size):
                 self._output_string += str(self._pixel_values[i][j])
@@ -83,43 +73,140 @@ class PracticeImage:
 
     # and two image manipulation functions;
     # flip (horizontal mirror) and flop (vertical mirror).
-    def flip(self):
-        pass
+    def flip(self) -> str:
+        """
+            Make a horizontal mirror of the pixel values referenced in the image object.
 
-    def flop(self):
-        pass
+                Parameters:
+                    None
+
+                Returns:
+                    Output image of the horizontal mirror in str format readable on the console,
+                    without affecting the original pixel values.
+        """
+        self._flipped_values = self._pixel_values[::-1]
+        # print("Manual horizontal flip")
+        # print(self._flipped_values)
+
+        for i in range(self._grid_size):
+            for j in range (self._grid_size):
+                self._flipped_output_string += str(self._flipped_values[i][j])
+            self._flipped_output_string += "\n"
+
+        return self._flipped_output_string
+
+    def flop(self) -> str:
+        """
+            Make a vertical mirror of the pixel values referenced in the image object.
+
+                Parameters:
+                    None
+
+                Returns:
+                    Output image of the vertical mirror in str format readable on the console,
+                    without affecting the original pixel values.
+        """
+        # result = [x[::-1] for x in myl]
+        self._flopped_values = [x[::-1] for x in self._pixel_values]
+
+        # print("Manual Vertical flop")
+        # print(self._flopped_values)
+
+        for i in range(self._grid_size):
+            for j in range(self._grid_size):
+                self._flopped_output_string += str(self._flopped_values[i][j])
+            self._flopped_output_string += "\n"
+
+        return self._flopped_output_string
+
 
     # as well as a fill function which should fill the image with empty pixels.
     def fill(self):
+        """
+            Fill the entire pixel array with black spaces as a way to clear all pixel values
 
+                Parameters:
+                    None
+
+                Returns:
+                    None
+        """
         for list in self._pixel_values:
             for i in range(self._grid_size):
-                list.append(i)
+                list.append(' ')
 
+class Triangle(PracticeImage):
+    # child class called Triangle that draws a right angle triangle
 
+    def __init__(self):
+        super().__init__()
+        self._triangle_output_string = ""
+
+    def draw_right_angle_triangle(self, symbol):
+
+        for i in range(self._grid_size):
+            self._pixel_values[i][0] = symbol
+            for j in range(self._grid_size):
+                self._pixel_values[self._grid_size-1][j] = symbol
+                if i == j:
+                    self._pixel_values[i][j] = symbol
+
+        for i in range(self._grid_size):
+            for j in range(self._grid_size):
+                self._triangle_output_string += str(self._pixel_values[i][j])
+            self._triangle_output_string += "\n"
+
+        return self._triangle_output_string
+
+        pass
 
 def main():
     # Initializing the image object
     image_object = PracticeImage()
-
+    #print(type(image_object))
     image_object.fill()
     # Gets the pixels & symbols
-    print("First get")
-    print(image_object.pixel_info)
+    #print("First get")
+    #print(image_object.pixel_info)
 
     # As a test, output an empty image
     #print(image_object.output_image())
+    # getting_image_info = image_object.pixel_info
 
-
-
+    # Calling first setter function to set pixel values and symbol
     image_object.pixel_info = (0,0,'#')
     # Gets the pixels & symbols
-    print("Second get")
+    # print("Second get")
     print(image_object.pixel_info)
+    print("Output image:")
     print(image_object.output_image())
+    flipped_output = image_object.flip()
+    print('Flipped output image')
+    print(flipped_output)
+
+    flopped_output = image_object.flop()
+    print('Flopped output image')
+    print(flopped_output)
+
+    # Making right angled triangle
+
+    right_angled_triangle = Triangle()
+    right_angled_triangle.fill()
+
+    # As a test, output an empty image
+    print("Test output from drawing right angled triangle")
+    print(right_angled_triangle.draw_right_angle_triangle('#'))
+
+    print("Test right angled triangle flip")
+    print(right_angled_triangle.flip())
+    print("Test right angled triangle flop")
+    print(right_angled_triangle.flop())
 
 
-    pass
+
+
+
+
 
 
 # main entry point to the program
--- test_question_1.py
+++ test_question_1.py
@@ -1,16 +1,131 @@
 from question_1 import PracticeImage
+from question_1 import Triangle
 import unittest
+import numpy as np
+
+# pytest test_question_1.py --cov-report term-missing --cov -v
+sample_triangle_image = """#         
+##        
+# #       
+#  #      
+#   #     
+#    #    
+#     #   
+#      #  
+#       # 
+##########
+"""
+# python -m unittest discover
 
 
 class TestPixels(unittest.TestCase):
     # tests go here
+    # python -m unittest test_question_1.TestPixels.test_if_variables_are_initialized_correctly
+    def test_if_variables_are_initialized_correctly(self):
+        test_image_object = PracticeImage()
+        self.assertEqual(test_image_object._grid_size, 10)
 
-    def test_getter_gets_size_and_symbol(self):
+    # python -m unittest test_question_1.TestPixels.test_if_getter_method_returns_values
+    def test_if_getter_method_returns_values(self):
+        test_image_object = PracticeImage()
+        self.assertIsNotNone(test_image_object.pixel_info)
+
+    # python -m unittest test_question_1.TestPixels.test_if_setter_method_sets_values
+    def test_if_setter_method_sets_values(self):
+        test_image_object = PracticeImage()
+        test_image_object.fill()
+        test_image_object.pixel_info = (0, 0, '#')
+        self.assertEqual(test_image_object._pixel_values[0][0], '#')
+
+    # python -m unittest test_question_1.TestPixels.test_if_output_image_returns_10x10_image
+    def test_if_output_image_returns_10x10_image(self):
+        test_image_object = PracticeImage()
+        test_image_object.fill()
+        image_string = test_image_object.output_image()
+        self.assertEqual(len(image_string), 110)
+
+    def test_flip_method_works(self):
+        test_numpy_string = ""
+        test_image_object = PracticeImage()
+        test_image_object.fill()
+        test_getting_image_info = test_image_object.pixel_info
+        test_image_object.pixel_info = (0, 0, '#')
+        flipped_test_image = test_image_object.flip()
+
+        numpy_flipped_image = np.flip(test_getting_image_info[0], 0)
+        # new_numpy_image = list(map(int, numpy_flipped_image))
+        # print("Manual image")
+        # print(test_getting_image_info[0])
+
+        # print("Manual string")
+        test_output_string = test_image_object.output_image()
+        # print(test_output_string)
+
+        # print("Manual flipped string")
+        # print(flipped_test_image)
+
+        # print("Numpy flipped list image")
+        list_numpy_flipped_image = numpy_flipped_image.tolist()
+        # print(list_numpy_flipped_image)
+        for i in range(test_image_object._grid_size):
+            for j in range(test_image_object._grid_size):
+                test_numpy_string += str(list_numpy_flipped_image[i][j])
+            test_numpy_string += "\n"
+
+        # print("Numpy flipped string")
+        # print(test_numpy_string)
+        self.assertEqual(flipped_test_image, test_numpy_string)
+
+    def test_flop_method_works(self):
+        test_numpy_string = ""
+        test_image_object = PracticeImage()
+        test_image_object.fill()
+        test_getting_image_info = test_image_object.pixel_info
+        test_image_object.pixel_info = (0, 0, '#')
+        flopped_test_image = test_image_object.flop()
+        # myl_flip_v = np.flip(myl,1) # vertical flip
+        numpy_flopped_image = np.flip(test_getting_image_info[0], 1)
+        # new_numpy_image = list(map(int, numpy_flopped_image))
+        # print("Manual image")
+        # print(test_getting_image_info[0])
+
+        # print("Manual string")
+        test_output_string = test_image_object.output_image()
+        # print(test_output_string)
+
+        # print("Manual flopped string")
+        # print(flopped_test_image)
+
+        # print("Numpy flipped list image")
+        list_numpy_flopped_image = numpy_flopped_image.tolist()
+        # print(list_numpy_flipped_image)
+        for i in range(test_image_object._grid_size):
+            for j in range(test_image_object._grid_size):
+                test_numpy_string += str(list_numpy_flopped_image[i][j])
+            test_numpy_string += "\n"
+
+        # print("Numpy flopped string")
+        # print(test_numpy_string)
+        self.assertEqual(flopped_test_image, test_numpy_string)
+
+    def test_if_triangle_variables_are_initialized_correctly(self):
+        test_right_angled_triangle = Triangle()
+        self.assertEqual(test_right_angled_triangle._triangle_output_string, '')
+
+    def test_draw_right_angle_triangle_works(self):
+        test_right_angled_triangle = Triangle()
+        test_right_angled_triangle.fill()
+        test_output_triangle = test_right_angled_triangle.draw_right_angle_triangle('#')
+        # print("Test output of triangle")
+        # print(test_output_triangle)
+        # print('Sample triangle image')
+        # print(sample_triangle_image)
+        self.assertEqual(test_output_triangle, sample_triangle_image)
+
+
+    def test_main_function(self):
+
         pass
-
-    def test_setter_sets_size_and_symbol(self):
-        pass
-
 
     def test_first_pixel_exists(self):
         pass
